1607b6ca9b614bce5f0b52a134add1c2d0df5615
EC2 Default User <ec2-user@ip-172-16-148-244.ec2.internal>
2020-08-06 03:35:54 +0000

    working dir

--- lambda_code/free_trial_collect_dayofweek.py
+++ lambda_code/free_trial_collect_dayofweek.py
@@ -7,7 +7,7 @@ logger.setLevel(logging.INFO)
 def lambda_handler(event, context):
     dayofweek = datetime.datetime.today().weekday()
     
-    if dayofweek == 2: # Sunday is 6
+    if dayofweek == 3: # Sunday is 6
         retrain = 'True'
     else:
         retrain = 'False'
